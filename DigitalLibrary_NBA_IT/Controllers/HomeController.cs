272b5145b284c16b8948fcae5e5d9b251da5e5ca
Idantegave7 <158479599+Idantegave7@users.noreply.github.com>
2024-12-16 18:04:35 +0200

    עדכון

--- DigitalLibrary_NBA_IT/Controllers/HomeController.cs
+++ DigitalLibrary_NBA_IT/Controllers/HomeController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DigitalLibrary_NBA_IT.Models;
+
 
 namespace DigitalLibrary_NBA_IT.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["Digital_library_DBEntities"].ConnectionString;
+
         public ActionResult Index()
         {
          
