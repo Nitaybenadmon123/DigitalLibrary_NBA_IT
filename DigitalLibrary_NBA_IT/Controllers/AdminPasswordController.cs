e9d8cfa3fdb1dabb5ec5e2562b85cb3b07684b12
Nitaybenadmon123 <nitaybe@ac.sce.ac.il>
2024-12-19 20:39:53 +0200

    סיסמה של המנהל

---
+++ DigitalLibrary_NBA_IT/Controllers/AdminPasswordController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BCrypt.Net;
+
+namespace DigitalLibrary_NBA_IT.Controllers
+{
+    public class AdminPasswordController : Controller
+    {
+        // GET: AdminPassword
+        public ActionResult GeneratePassword()
+        {
+            // סיסמה רגילה להצפנה
+            string plainPassword = "ADMIN123@";
+
+            // הצפנת הסיסמה
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(plainPassword);
+
+            // הצגת התוצאה בדפדפן
+            return Content($"The hashed password is: {hashedPassword}");
+        }
+    }
+}
